56228c3c689340788241070d2623a7cc91cf3a7b
Pornmongkon Pongsai <pornmongkonp@riverpark.co.th>
2017-05-24 22:50:46 +0700

    Adding Some CSS Styles

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-var app = angular.module('flapperNews', []);
-
-app.controller('MainCtrl', [
-    '$scope',
-    function($scope) {
-        $scope.test = 'Hello world!';
-        $scope.posts = [
-            { title: 'post 1', upvotes: 5 },
-            { title: 'post 2', upvotes: 2 },
-            { title: 'post 3', upvotes: 15 },
-            { title: 'post 4', upvotes: 9 },
-            { title: 'post 5', upvotes: 4 }
-        ];
-
-        $scope.addPost = function() {
-            $scope.posts.push({
-                title: $scope.title,
-                upvotes: 0,
-                link: $scope.link
-            });
-            $scope.title = '';
-            $scope.link = '';
-        }
-
-        $scope.incrementUpvotes = function(post) {
-            post.upvotes += 1;
+angular.module('flapperNews', [])
+    .factory('posts', [function() {
+        var obj = {
+            posts: [{
+                title: 'hello',
+                link: '',
+                upvotes: 5
+            }]
         };
-    }
-]);
+        return obj;
+    }])
+    .controller('MainCtrl', [
+        '$scope', 'posts',
+        function($scope, posts) {
+            $scope.test = 'Hello world!';
+
+            $scope.posts = posts.posts;
+
+            $scope.addPost = function() {
+                $scope.posts.push({
+                    title: $scope.title,
+                    upvotes: 0,
+                    link: $scope.link
+                });
+                $scope.title = '';
+                $scope.link = '';
+            }
+
+            $scope.incrementUpvotes = function(post) {
+                post.upvotes += 1;
+            };
+        }
+    ]);
