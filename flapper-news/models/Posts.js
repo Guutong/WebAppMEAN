897be192dafae3bf3ce369dcaaa5577a85752b67
Pornmongkon Pongsai <pornmongkonp@riverpark.co.th>
2017-05-26 17:09:49 +0700

    Finishing Off Comments

--- flapper-news/models/Posts.js
+++ flapper-news/models/Posts.js
@@ -4,15 +4,13 @@ var PostSchema = new mongoose.Schema({
     title: String,
     link: String,
     upvotes: { type: Number, default: 0 },
-    comment: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Comment'
-    }]
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+    author: String
 });
 
 PostSchema.methods.upvote = function(cb) {
     this.upvotes += 1;
     this.save(cb);
-}
+};
 
 mongoose.model('Post', PostSchema);
--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -17,7 +17,12 @@ var app = angular.module('flapperNews', ['ui.router'])
                 .state('posts', {
                     url: '/posts/{id}',
                     templateUrl: '/posts.html',
-                    controller: 'PostsCtrl'
+                    controller: 'PostsCtrl',
+                    resolve: {
+                        post: ['$stateParams', 'posts', function($stateParams, posts) {
+                            return posts.get($stateParams.id);
+                        }]
+                    }
                 });
             $urlRouterProvider.otherwise('home');
         }
@@ -26,16 +31,24 @@ var app = angular.module('flapperNews', ['ui.router'])
         var obj = {
             posts: []
         };
+        obj.get = function(id) {
+            return $http.get('/posts/' + id).then(function(res) {
+                return res.data
+            });
+        }
+
         obj.getAll = function() {
             return $http.get('/posts').success(function(data) {
                 angular.copy(data, obj.posts);
             });
         }
+
         obj.create = function(post) {
             return $http.post('/posts', post).success(function(data) {
                 obj.posts.push(data);
             });
         };
+
         obj.upvote = function(post) {
             return $http.put('/posts/' + post._id + '/upvote')
                 .success(function(data) {
@@ -43,11 +56,21 @@ var app = angular.module('flapperNews', ['ui.router'])
                 });
         };
 
+        obj.addComment = function(id, comment) {
+            return $http.post('/posts/' + id + '/comments', comment);
+        };
+
+        obj.upvoteComment = function(post, comment) {
+            return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
+                .success(function(data) {
+                    comment.upvotes += 1;
+                });
+        };
         return obj;
     }])
     .controller('MainCtrl', [
         '$scope', 'posts',
-        function($scope, posts) {
+        function($scope, posts, post) {
             $scope.test = 'Hello world!';
             $scope.posts = posts.posts;
             $scope.addPost = function() {
@@ -65,19 +88,21 @@ var app = angular.module('flapperNews', ['ui.router'])
         }
     ])
     .controller('PostsCtrl', [
-        '$scope', '$stateParams', 'posts',
-        function($scope, $stateParams, posts) {
-            $scope.post = posts.posts[$stateParams.id];
+        '$scope', '$stateParams', 'posts', 'post',
+        function($scope, $stateParams, posts, post) {
+            $scope.post = post;
             $scope.addComment = function() {
-                if ($scope.body === '') {
-                    return;
-                }
-                $scope.post.comments.push({
+                if ($scope.body === '') { return; }
+                posts.addComment(post._id, {
                     body: $scope.body,
                     author: 'user',
-                    upvotes: 0
+                }).success(function(comment) {
+                    $scope.post.comments.push(comment);
                 });
                 $scope.body = '';
-            }
+            };
+            $scope.incrementUpvotes = function(comment) {
+                posts.upvoteComment(post, comment);
+            };
         }
     ]);
